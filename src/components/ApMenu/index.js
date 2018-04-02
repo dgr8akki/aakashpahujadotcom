ccba2cad1e7e4c75dc356a3861ff5e0f4d6c4426
Aakash Pahuja <aakash.pahuja@kony.com>
2018-04-03 00:31:41 +0530

    Add menu links

--- src/components/ApMenu/index.js
+++ src/components/ApMenu/index.js
@@ -1,16 +1,68 @@
-import React, { Component } from 'react'
-import { Icon } from 'semantic-ui-react'
+import React, { Component } from "react";
+import "./index.css";
 
+const animationOnMenuClick = e => {
+  document
+    .getElementsByClassName("menu__item--current")[0]
+    .classList.remove("menu__item--current");
+  e.target.parentElement.classList.add("menu__item--current");
+};
 class ApMenu extends Component {
   render() {
     return (
       <div id="menu">
-        <Icon name='bars' size="large" />
-        <span>Menu</span>
+        <nav className="menu menu--miranda">
+          <ul className="menu__list">
+            <li className="menu__item menu__item--current">
+              <a
+                href="#"
+                className="menu__link"
+                onClick={e => animationOnMenuClick(e)}
+              >
+                home
+              </a>
+            </li>
+            <li className="menu__item">
+              <a
+                href="#"
+                className="menu__link"
+                onClick={e => animationOnMenuClick(e)}
+              >
+                my work
+              </a>
+            </li>
+            <li className="menu__item">
+              <a
+                href="#"
+                className="menu__link"
+                onClick={e => animationOnMenuClick(e)}
+              >
+                experience
+              </a>
+            </li>
+            <li className="menu__item">
+              <a
+                href="#"
+                className="menu__link"
+                onClick={e => animationOnMenuClick(e)}
+              >
+                skills
+              </a>
+            </li>
+            <li className="menu__item">
+              <a
+                href="#"
+                className="menu__link"
+                onClick={e => animationOnMenuClick(e)}
+              >
+                contact
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
-    )
+    );
   }
 }
 
 export default ApMenu;
-
