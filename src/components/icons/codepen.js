3cdfff3b2d82f3d57285bcaa695fd57f3fa959ae
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-11-05 23:05:06 +0530

    [Portfolio] Refactor

--- src/components/icons/codepen.js
+++ src/components/icons/codepen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const IconCodepen = () => (
+export default IconCodepen = () => (
   <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 31.665 31.665">
     <title>Codepen</title>
     <path
@@ -13,5 +13,3 @@ const IconCodepen = () => (
     />
   </svg>
 );
-
-export default IconCodepen;
