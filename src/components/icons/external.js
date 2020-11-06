12d40a19562ca5587c40ad8bd133012399957bf3
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-11-06 23:05:21 +0530

    [Portfolio] Refactor

--- src/components/icons/external.js
+++ src/components/icons/external.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const IconExternal = () => (
+export default IconExternal = () => (
   <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 194.818 194.818">
     <title>External</title>
     <g>
@@ -16,5 +16,3 @@ const IconExternal = () => (
     </g>
   </svg>
 );
-
-export default IconExternal;
