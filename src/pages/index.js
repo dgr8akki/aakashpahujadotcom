2e8250fcaf38f9e6141c6cb3f6667c64b1fe97b9
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-11-10 23:06:02 +0530

    [Portfolio] Refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,11 +23,6 @@ const IndexPage = ({ location, data, darkMode, enableDarkMode, lang, changeLangu
       <Projects data={data.projects.edges} />
       <Contact data={data.contact.edges} />
     </StyledMainContainer>
-    {/* <button
-      style={darkMode ? { backgroundColor: 'black', color: 'white' } : null}
-      onClick={() => enableDarkMode(!darkMode)}>
-      Dark Mode is {darkMode ? 'On' : 'Off'}
-    </button> */}
   </Layout>
 );
 
