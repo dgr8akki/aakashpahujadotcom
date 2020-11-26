212901decfe339bfad02b198121b365bc63bd3e7
Aakash Pahuja <pahujaaakash5@gmail.com>
2020-11-26 23:10:45 +0530

    [Portfolio] Refactor

--- src/styles/Footer.js
+++ src/styles/Footer.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import { theme } from './theme';
 
+const {margin} = theme;
+
 const Footer = styled.footer`
   margin: 0;
-  padding: ${theme.margin};
+  padding: ${margin};
 `;
 
 export default Footer;
