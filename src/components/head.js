8a334ac485594987bdcc4a15db267348e69bb0e1
Aakash Pahuja <pahujaaakash5@gmail.com>
2022-10-14 16:28:51 +0100

    Update head.js

--- src/components/head.js
+++ src/components/head.js
@@ -37,6 +37,17 @@ const Head = ({ metadata }) => (
     <meta name="twitter:image:alt" content={metadata.title} />
     <meta name="msapplication-TileColor" content={config.navyColor} />
     <meta name="theme-color" content={config.navyColor} />
+    <script>
+      window.intercomSettings = {
+        api_base: "https://api-iam.intercom.io",
+        app_id: "tbjqrlfb"
+      };
+    </script>
+
+    <script>
+    // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/tbjqrlfb'
+    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/tbjqrlfb';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    </script>
   </Helmet>
 );
 
