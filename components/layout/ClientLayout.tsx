76a5b0ad991397bc20297dce064820ba72eb52b2
Aakash Pahuja <apahuja@squarespace.com>
2026-01-14 13:12:52 +0000

    Remove loader

--- components/layout/ClientLayout.tsx
+++ components/layout/ClientLayout.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { useState, useEffect } from 'react';
-import { Loader } from '@/components/ui/Loader';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
 import { SideSocial } from './SideSocial';
@@ -12,38 +10,13 @@ interface ClientLayoutProps {
 }
 
 export function ClientLayout({ children }: ClientLayoutProps) {
-  const [isLoading, setIsLoading] = useState(true);
-  const [showContent, setShowContent] = useState(false);
-
-  useEffect(() => {
-    // Check if this is the first visit in this session
-    const hasLoaded = sessionStorage.getItem('hasLoaded');
-    if (hasLoaded) {
-      setIsLoading(false);
-      setShowContent(true);
-    }
-  }, []);
-
-  const handleLoaderFinish = () => {
-    setIsLoading(false);
-    setShowContent(true);
-    sessionStorage.setItem('hasLoaded', 'true');
-  };
-
   return (
-    <>
-      {isLoading && <Loader onFinish={handleLoaderFinish} />}
-      <div
-        className={`min-h-screen flex flex-col transition-opacity duration-300 ${
-          showContent ? 'opacity-100' : 'opacity-0'
-        }`}
-      >
-        <Navbar />
-        <SideSocial />
-        <SideEmail />
-        <main className="flex-1">{children}</main>
-        <Footer />
-      </div>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <SideSocial />
+      <SideEmail />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
   );
 }
--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -29,33 +29,3 @@ export function Logo({ className = '' }: LogoProps) {
     </svg>
   );
 }
-
-export function LogoLoader({ className = '' }: LogoProps) {
-  return (
-    <svg
-      id="logo"
-      width="100"
-      height="115"
-      viewBox="0 0 220 253"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-    >
-      <path
-        d="M110.058 4.125L4 63.9528V186.328L110.058 248.875L216.117 189.047V66.6722L110.058 4.125Z"
-        fill="#000000"
-        stroke="#FF9E64"
-        strokeWidth="7"
-        strokeDasharray="1000"
-        strokeDashoffset="1000"
-        className="animate-[dash_2s_ease-in-out_forwards]"
-      />
-      <path
-        id="B"
-        d="M107.826 85.7122H121.826L154.866 171.672H140.026L131.206 147.872H98.3061L89.4861 171.672H74.7861L107.826 85.7122ZM114.686 103.492L102.926 135.272H126.446L114.686 103.492Z"
-        fill="#FF9E64"
-        className="opacity-0 animate-[fadeIn_0.8s_ease-in-out_1.5s_forwards]"
-      />
-    </svg>
-  );
-}
